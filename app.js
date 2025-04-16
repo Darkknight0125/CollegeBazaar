85279e42ddd92e112a49e498ae7fdfb0641e7a10
Darkknight0125 <samansidhu0125@gmail.com>
2025-04-16 22:46:39 +0530

    Auth middleware added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
+import authMiddleware from './middlewares/authMiddleware.js';
 
 dotenv.config();
 
@@ -10,6 +11,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+
+app.get('/profile', authMiddleware, (req, res) => {
+  // req.user is now available
+  res.json({ message: 'Authenticated user', user: req.username, user_id: req.user_id });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
