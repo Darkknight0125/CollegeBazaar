f2b542d10734d88a4b09b683d369896fed1afaab
Darkknight0125 <samansidhu0125@gmail.com>
2025-04-16 22:42:29 +0530

    Login route addded

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 import bcrypt from 'bcrypt';
 import db from '../config/db.js';
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async (req, res) => {
 
@@ -37,3 +39,41 @@ export const signup = async (req, res) => {
   }
   
 };
+
+
+export const login = async (req, res) => {
+
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(400).json({ error: 'Email and password are required' });
+
+  try {
+
+    const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+
+    if (result.rows.length === 0)
+      return res.status(401).json({ error: 'Invalid credentials' });
+
+    const user = result.rows[0];
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match)
+      return res.status(401).json({ error: 'Invalid credentials' });
+
+    const token = jwt.sign({ 
+      user_id: user.user_id,
+      username: user.name
+    }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+    res.status(200).json({ token });
+
+  } 
+  catch (err) {
+    console.error('Login error:', err);
+    res.status(500).json({ error: 'Server error during login' });
+  }
+  
+};
+
